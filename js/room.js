eeacfcadea323fa0fc696faea4fe8d3d6c2b8077
Thaina Luzia <thaina.lvcardoso@gmail.com>
2023-10-10 23:20:43 -0300

    fix: erros, nomes, adicionei  label

--- js/room.js
+++ js/room.js
@@ -2,11 +2,12 @@ const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal"); 
 const modal = document.querySelector("#modal"); 
 const fade = document.querySelector("#fade"); 
-const listaDeSalas = document.querySelector("#room-list");
+const listaDeTurma = document.querySelector("#room-list");
 const roomName = document.querySelector("#room_name")
 const roomNumber = document.querySelector("#room_number")
 const roomFloor = document.querySelector("#room_floor")
 const emptyMessgeRoom = document.querySelector("#empty-message-room")
+const URL = "https://v2csj5c0-3000.brs.devtunnels.ms/"
 
 const toggleModal = () => {
     modal.classList.toggle("hide");
@@ -39,28 +40,28 @@ function cleanRoomModalFields(){
     roomFloor.value = null
 }
 
-function createRoomComponent(nomeSala, numero, qtdAlunos, andar) {
+function createRoomComponent(nomeTurma, numero, qtdAlunos, andar) {
     const pai = document.createElement("li")
     const titulo = document.createElement("h2")
     const texto = document.createElement("p")
 
-    titulo.innerHTML = nomeSala
-    texto.innerHTML = "Sala: " + numero + "<br/>" + "Alunos: " + qtdAlunos + "<br/>" + "Andar: " + andar
+    titulo.innerHTML = nomeTurma
+    texto.innerHTML = "Turma: " + numero + "<br/>" + "Alunos: " + qtdAlunos + "<br/>" + "Andar: " + andar
 
     pai.className = "folder-componet"
     pai.appendChild(titulo)
     pai.appendChild(texto)
-    listaDeSalas.appendChild(pai)
+    listaDeTurma.appendChild(pai)
 }
 
 function fetchRooms() {
-    fetch("https://mocki.io/v1/710708d1-2f91-4e24-8f3d-774d559f1071").then(res => res.json()).then(salas => {
-        for (let index = 0; index < salas.length; index++) {
-            const sala = salas[index];
-            createRoomComponent(sala.name, sala.number, sala.qtdStudents, sala.floor)
+    fetch(URL + "rooms").then(res => res.json()).then(turmas => {
+        for (let index = 0; index < turmas.length; index++) {
+            const turma = turmas[index];
+            createRoomComponent(turma.NomeTurma, turma.number, turma.qtdStudents, turma.floor)
         }
         emptyMessgeRoom.classList.add("hide")
     })
 }
 
-onLoad();
+onLoad(); 
